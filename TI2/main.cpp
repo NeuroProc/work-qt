2afcd4133cac32c334ce22c4f270ca50ba08132c
NeuroProc <ivanpystakov@gmail.com>
2014-10-24 21:53:23 +0300

    TI2-1.1-RELEASE

--- TI2/main.cpp
+++ TI2/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-u_long LSFSR(FILE *C, FILE *M, FILE *Kf);
+u_long LFSR(FILE *C, FILE *M, FILE *Kf);
 u_long GEFFE(FILE *C, FILE *M, FILE *Kf);
 u_long RC4(FILE *C, FILE *M, FILE *Kf);
 void swapChar(char &, char &);
@@ -28,13 +28,13 @@ int main(int argc, char *argv[])
     int numb;
     std::bitset<sizeof(u_long) * 8> endState;
 
-    cout << "1 - LSFSR" << endl << "2 - GEFFE" << endl << "3 - RC4"  << endl <<  "Enter crypto algorithm: ";
+    cout << "1 - LFSR" << endl << "2 - GEFFE" << endl << "3 - RC4"  << endl <<  "Enter crypto algorithm: ";
     cin >> numb;
 
     switch (numb)
     {
     case 1:
-        endState = LSFSR(C, M, K);
+        endState = LFSR(C, M, K);
         break;
     case 2:
         endState = GEFFE(C, M, K);
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-u_long LSFSR(FILE *C, FILE *M, FILE *Kf)
+u_long LFSR(FILE *C, FILE *M, FILE *Kf)
 {
     char K;
     u_long x25, x3, x0;
@@ -66,7 +66,6 @@ u_long LSFSR(FILE *C, FILE *M, FILE *Kf)
     x0 = (u_long) 1;
     u_long longK;
     u_long mask25 = 0;//25 - битовая маска (cдвиговый регистр, длина которого равна степени многочлена)
-    u_long bit25 = ((u_long) 1) << 25;//маска для извлечения 25 бита
     for( int i = 0; i < 25; i++ )
     {
         mask25 = mask25 | x0;
@@ -79,31 +78,35 @@ u_long LSFSR(FILE *C, FILE *M, FILE *Kf)
     cin >> key;
 
     cout << "U REG: " << key << endl;
+    longK = key.to_ulong();
+    K = (char)(longK >> 17);
 
-    K = (char)key.to_ulong();
-
-    longK = (u_long) K;     //
+    std::bitset<8> keyBin;
 
     char inputsymbol = fgetc(M);
     while( !feof(M) )
     {
         char outputsymbol = K ^ inputsymbol;
-
         fputc(outputsymbol, C );
-        fputc(K, Kf);
 
-        u_long temp = 0;
-        if( longK & x25 )
-            temp = temp ^ (u_long)1;
-        if( longK & x3 )
-            temp = temp ^ (u_long)1;
+        keyBin = K;
+        fprintf(Kf, "%s ", keyBin.to_string().c_str());
 
-        longK = (longK << 1) & mask25;
-        longK = longK | temp;
+//        cout << keyBin << endl;
 
-        K = K << 1;
-        if( temp == 1 )
-            K = K | (char)1;
+        for (int i = 0; i < 8; i++)
+        {
+            u_long temp = 0;
+            if( longK & x25 )
+                temp = temp ^ (u_long)1;
+            if( longK & x3 )
+                temp = temp ^ (u_long)1;
+
+            longK = (longK << 1) & mask25;
+            longK = longK | temp;
+
+        }
+        K = (char)(longK >> 17);
 
         inputsymbol = fgetc(M);
     }
@@ -112,9 +115,9 @@ u_long LSFSR(FILE *C, FILE *M, FILE *Kf)
 
 u_long GEFFE(FILE *C, FILE *M, FILE *Kf)
 {
-    char K1, K2, K3, Kg;
+    char Kg;
     u_long x25, x3, x0, x33, x13, x23, x5;
-    // x - члены многочлена для вставки первого чимвола
+
     x25 = ((u_long)1) << 24;
     x3 = ((u_long)1) << 2;
 
@@ -124,30 +127,28 @@ u_long GEFFE(FILE *C, FILE *M, FILE *Kf)
     x23 = ((u_long)1) << 22;
     x5 = ((u_long)1) << 4;
 
-    x0 = (u_long) 1;
-
-
-    u_long longK1, longK2, longK3, longKg;
+    u_long longK1, longK2, longK3;
     u_long mask25 = 0, mask33 = 0, mask23 = 0;//23, 31, 39 - битовая маска
-    u_long bit25 = ((u_long) 1) << 24, bit33 = ((u_long) 1) << 32, bit23 = ((u_long) 1) << 22;//маски для извлечения 24, 32  и 40 битов
-    for( int i = 0; i < 24; i++ )
+
+    x0 = (u_long)1;
+    for( int i = 0; i < 25; i++ )
     {
      mask25 = mask25 | x0;
      x0 = x0 << 1;
     }
-    mask33 = mask25;
-    for( int i = 26; i < 32; i++ )
+    x0 = (u_long)1;
+    for( int i = 0; i < 33; i++ )
     {
      mask33 = mask33 | x0;
      x0 = x0 << 1;
     }
-    for( int i = 0; i < 22; i++ )
+    x0 = (u_long)1;
+    for( int i = 0; i < 23; i++ )
     {
      mask23 = mask23 | x0;
      x0 = x0 << 1;
     }
-    x0 = (u_long) 1;
-
+    x0 = (u_long)1;
 
     std::bitset<25> key1;
     std::bitset<33> key2;
@@ -155,10 +156,8 @@ u_long GEFFE(FILE *C, FILE *M, FILE *Kf)
 
     cout << "Enter start register N1: ";
     cin >> key1;
-
     cout << "Enter start register N2: ";
     cin >> key2;
-
     cout << "Enter start register N3: ";
     cin >> key3;
 
@@ -166,74 +165,109 @@ u_long GEFFE(FILE *C, FILE *M, FILE *Kf)
     cout << "U REG2: " << key2 << endl;
     cout << "U REG3: " << key3 << endl;
 
-    K1 = (char)key1.to_ulong();
-    K2 = (char)key2.to_ulong();
-    K3 = (char)key3.to_ulong();
+    longK1 = key1.to_ulong();
+    longK2 = key2.to_ulong();
+    longK3 = key3.to_ulong();
+
 
     char inputsymbol = fgetc(M);
-    Kg = (K1 & K2) | ~(K1 & K3);
-    longKg = (longK1 & longK2) | ~( longK1 & longK3 );
+    Kg = (char)(( (longK1 >> 17) & (longK2 >> 25) ) | ( ~(longK1 >> 17) & (longK3 >> 15) ));
+
+    std::bitset<8> keyBin;
+
     while( !feof(M) )
     {
-     fputc( (Kg ^ inputsymbol), C );
-     fputc(Kg, Kf);
+        fputc( (Kg ^ inputsymbol), C );
 
-     u_long temp = 0;
-     if( longK1 & x25 )
-         temp = temp ^ (u_long)1;
-     if( longK1 & x3 )
-         temp = temp ^ (u_long)1;
-     longK1 = (longK1 << 1) & mask25;
-     longK1 = longK1 | temp;
-     K1 = K1 << 1;
-     if( temp == 1 )
-         K1 = K1 | (char)1;
+        keyBin = Kg;
+        fprintf(Kf, "%s ", keyBin.to_string().c_str());
 
-     temp = 0;
-     if( longK2 & x33 )
-         temp = temp ^ (u_long)1;
-     if( longK2 & x13 )
-         temp = temp ^ (u_long)1;
-     longK2 = (longK2 << 1) & mask33;
-     longK2 = longK2 | temp;
-     K2 = K2 << 1;
-     if( temp == 1 )
-         K2 = K2 | (char)1;
+//        cout << keyBin << endl;
 
-     temp = 0;
-     if( longK3 & x23 )
-         temp = temp ^ (u_long)1;
-     if( longK3 & x5 )
-         temp = temp ^ (u_long)1;
-     longK3 = (longK3 << 1) & mask23;
-     longK3 = longK3 | temp;
-     K3 = K3 << 1;
-     if( temp == 1 )
-         K3 = K3 | (char)1;
+        for (int i = 0; i < 8; i++)
+        {
+            u_long temp = 0;
+            if( longK1 & x25 )
+                temp = temp ^ (u_long)1;
+            if( longK1 & x3 )
+                temp = temp ^ (u_long)1;
+            longK1 = (longK1 << 1) & mask25;
+            longK1 = longK1 | temp;
 
-     Kg = (K1 & K2) | ~(K1 & K3);
-     longKg = (longK1 & longK2) | ~( longK1 & longK3 );
+/*
+            std::bitset<64> tst;
+            tst = longK2;
+            cout << "DEBUG: " << tst << endl;
+*/
 
-     inputsymbol = fgetc(M);
+            temp = 0;
+            if( longK2 & x33 )
+                temp = temp ^ (u_long)1;
+            if( longK2 & x13 )
+                temp = temp ^ (u_long)1;
+            longK2 = (longK2 << 1) & mask33;
+            longK2 = longK2 | temp;
+/*
+            tst = mask33;
+            cout << "DEBUG: " << tst << endl;
+*/
+
+            temp = 0;
+            if( longK3 & x23 )
+                temp = temp ^ (u_long)1;
+            if( longK3 & x5 )
+                temp = temp ^ (u_long)1;
+            longK3 = (longK3 << 1) & mask23;
+            longK3 = longK3 | temp;
+
+
+            std::bitset<64> key;
+            key = longK1 >> 8;
+            cout << "longK1[" << i << "]: " << key << endl;
+            key = longK2 >> 8;
+            cout << "longK2[" << i << "]: " << key << endl;
+            key = longK3 >> 8;
+            cout << "longK3[" << i << "]: " << key << endl << endl;
+
+
+        }
+
+        Kg = (char)(( (longK1 >> 17) & (longK2 >> 25) ) | ( ~(longK1 >> 17) & (longK3 >> 15) ));
+
+        inputsymbol = fgetc(M);
     }
     return (u_long)Kg;
 }
 
 u_long RC4(FILE *C, FILE *M, FILE *Kf)
 {
-    char userkey[100];
+    //char userkey[100];
+    string userkey;
     char Sbox[256];
     int count = 0;
 
+    int symbCount;
+    cout << "Enter count symbols: ";
+    cin >> symbCount;
+
     cout << "Enter key: ";
-    cin >> userkey;
+    for (int i = 0; i < symbCount; i++)
+    {
+        int symb;
+        cin >> symb;
+        userkey += (char)symb;
+    }
+    //cout << userkey << endl;
+
+    //cin >> userkey;
 
     for( int i= 0; i < 256; i++ )
          Sbox[i] = i;
     int j = 0;
     for( int i = 0; i < 256; i++)
     {
-         j = (j + (unsigned char)Sbox[i] + userkey[i % strlen(userkey)]) % 256;
+         //j = (j + (unsigned char)Sbox[i] + userkey[i % strlen(userkey.c_str())]) % 256;
+         j = (j + (unsigned char)Sbox[i] + userkey[i % symbCount]) % 256;
          swapChar( Sbox[i], Sbox[j] );
     }
 
@@ -254,6 +288,7 @@ u_long RC4(FILE *C, FILE *M, FILE *Kf)
         char newsym = (currentKey ^ symbol);
 
         fputc(newsym, C);
+        fprintf(Kf, "%d ", (int)((unsigned char)currentKey));
 
         symbol = fgetc(M);
     }
